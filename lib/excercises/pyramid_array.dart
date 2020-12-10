a1e930d804789bff8ee407b1c9013445570b80f9
Jorge Sarabia <jorge@sarabiajor.ge>
2020-12-10 20:42:09 -0300

    The first exercise placeholder

---
+++ lib/excercises/pyramid_array.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PyramidArray extends StatefulWidget {
+  const PyramidArray();
+
+  @override
+  _PyramidArrayState createState() => _PyramidArrayState();
+}
+
+class _PyramidArrayState extends State<PyramidArray> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pyramid Array'),
+      ),
+    );
+  }
+}
