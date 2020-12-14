b1999d4591ec9e271848ccfb4e7ac0a955b1ae5e
Jorge Sarabia <jorge@sarabiajor.ge>
2020-12-14 20:08:50 -0300

    WIP

--- lib/excercises/pyramid_array.dart
+++ lib/excercises/pyramid_array.dart
@@ -14,6 +14,44 @@ class _PyramidArrayState extends State<PyramidArray> {
       appBar: AppBar(
         title: Text('Pyramid Array'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              'Write a function that when given a number >= 0, returns an Array of ascending length subarrays.',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(
+                  const Radius.circular(10.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'one',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
