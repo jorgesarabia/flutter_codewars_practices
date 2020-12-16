2c5c951b13431e53cf7720ab68c781f9f13381db
Jorge Sarabia <jorge@sarabiajor.ge>
2020-12-16 19:23:52 -0300

    Show the test code

--- lib/excercises/pyramid_array/test_code.dart
+++ lib/excercises/pyramid_array/test_code.dart
@@ -7,6 +7,7 @@ class TestCode extends StatefulWidget {
 
 class _TestCodeState extends State<TestCode> {
   String solution = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -14,59 +15,65 @@ class _TestCodeState extends State<TestCode> {
       appBar: AppBar(
         title: Text('Test Code'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter a number',
-              ),
-              onChanged: (String number) {
-                final n = int.parse(number);
-                if (n < 0) {
-                  print('Show error message');
-                } else {
-                  setState(() {
-                    solution = showSolution(n);
-                  });
-                }
-              },
-            ),
-            SizedBox(height: 20.0),
-            if (solution.isNotEmpty)
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(
-                    const Radius.circular(10.0),
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter a number',
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    solution,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
+                onChanged: (String number) {
+                  final n = int.parse(number);
+                  if (n < 0) {
+                    solution = '';
+                    error = '(*) The number must be greater than zero';
+                  } else {
+                    solution = showSolution(n);
+                    error = '';
+                  }
+                  setState(() {});
+                },
+              ),
+              SizedBox(height: 20.0),
+              if (solution.isNotEmpty)
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(
+                      const Radius.circular(10.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      solution,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 15.0,
+                ),
               ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
-  String showSolution(int n) {
-    return 'pyramid($n) => ${pyramid(n)}';
-  }
+  String showSolution(int n) => 'pyramid($n):\n\n${pyramid(n)}';
 
-  List<List<int>> pyramid(int n) {
-    return List.generate(n, (i) => List.filled(i + 1, 1));
-  }
+  List<List<int>> pyramid(int n) => List.generate(n, (i) => List.filled(i + 1, 1));
 }
