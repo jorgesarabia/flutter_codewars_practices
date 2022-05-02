f3376b11b603c5164174a131413538bfce57aefc
Jorge Sarabia <jorge@sarabiajor.ge>
2022-05-02 17:40:11 -0300

    update

---
+++ .vscode/testing.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+getBranch(){
+	branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+	echo $branch
+}
+
+getIssue(){
+	localbranch=$(getBranch)
+	branchName=$(echo "$localbranch" | awk -F '/' '{print $2}')
+	issueId=$(echo "$branchName" | awk -F '_' '{print $1}')
+	echo "$issueId"
+}
+
+getMesage(){
+	message="$1\n\nIssue: $2\nBranch: $3\n\nJorge Sarabia - jorge.sarabia@apoyomultiple.com"
+	echo -e $message
+}
+
+getMesage "$1" "$(getIssue)" "$(getBranch)"
